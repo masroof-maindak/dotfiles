6aeb80feae4c985d7a9405c16d26195a9a125a7d
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-07-12 21:05:20 +0500

    chore: capitalise hex codes

--- .config/nvim/colors/swamp.lua
+++ .config/nvim/colors/swamp.lua
@@ -2,7 +2,7 @@ local palette
 
 if vim.o.background == "light" then
   palette = {
-    base00 = "#f1e3d1", -- BG
+    base00 = "#F1E3D1", -- BG
     base01 = "#DDCEBC", -- BG2
     base02 = "#C9B9A7", -- BG3
     base03 = "#B5A492", -- Comments
@@ -10,14 +10,14 @@ if vim.o.background == "light" then
     base05 = "#64513E", -- FG
     base06 = "#786653",
     base07 = "#8C7B68",
-    base08 = "#d09700", -- Variables
+    base08 = "#D09700", -- Variables
     base09 = "#64513E", -- numbers
     base0A = "#993333", -- Classes
-    base0B = "#908D6A", -- Strings
+    base0B = "#D08D6A", -- Strings
     base0C = "#d09700", -- Support
-    base0D = "#bf7979", -- Functions
-    base0E = "#9e5581", -- Keywords
-    base0F = "#75858c", -- Parentheses
+    base0D = "#BF7979", -- Functions
+    base0E = "#9E5581", -- Keywords
+    base0F = "#75858C", -- Parentheses
   }
 end
 
@@ -29,16 +29,16 @@ if vim.o.background == "dark" then
     base03 = "#5F4E41",
     base04 = "#B8A58C",
     base05 = "#D2C3A4",
-    base06 = "#ebe0bb",
+    base06 = "#EBE0BB",
     base07 = "#F1E9D0",
-    base08 = "#db930d",
-    base09 = "#ebe0bb",
-    base0A = "#a82d56",
-    base0B = "#7a7653",
-    base0C = "#db930d",
-    base0D = "#c1666b",
-    base0E = "#91506c",
-    base0F = "#61a0a8",
+    base08 = "#DB930D",
+    base09 = "#EBE0BB",
+    base0A = "#A82D56",
+    base0B = "#7A7653",
+    base0C = "#DB930D",
+    base0D = "#C1666B",
+    base0E = "#91506C",
+    base0F = "#61A0A8",
   }
 end
 
