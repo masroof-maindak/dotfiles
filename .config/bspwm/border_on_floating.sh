0837c86ee963bf7ec4e786b928a60e9ad0666959
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-07-30 21:49:50 +0500

    patch: set default border height to 1
    All script dilemmas resolved. no excess code/performance impact.

--- .config/bspwm/border_on_floating.sh
+++ .config/bspwm/border_on_floating.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 FLOATING_BORDER_WIDTH=10
-BORDER_WIDTH=0
+BORDER_WIDTH=1
 
 bspc subscribe node_state | while read msg monitor desktop wid state value ; do
 if [ $state = "floating" ] ; then
     if [ $value = "on" ] ; then
         bspc config -n $wid border_width $FLOATING_BORDER_WIDTH
-    else
+    else # Used to reset the border width when the window is no longer floating
         bspc config -n $wid border_width $BORDER_WIDTH
     fi
 fi
--- .config/bspwm/border_on_focused.sh
+++ .config/bspwm/border_on_focused.sh
@@ -3,6 +3,6 @@
 BORDER_WIDTH=10
 
 while bspc subscribe -c 1 node_focus; do
-	bspc config border_width 0
+	bspc config border_width 1
 	bspc config -n focused border_width $BORDER_WIDTH
 done
