413b5e22f97f3f894b9b1489ac325814b86bf233
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-08-03 12:51:28 +0500

    refactor: formatting and moved some files around

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,6 @@ source utils.sh
 
 install_paru
 
-# Make hardcoded directories
 print_yellow "Making directories"
 mkdir -p "$HOME"/{Screenshots,Desktop,Documents,Downloads,Music,Pictures/Wallpapers}
 mkdir -p "$HOME"/{.local/bin,.themes,.icons,.config/vesktop/settings}
@@ -20,11 +19,7 @@ install_rust
 
 # Install packages
 installList "base"
-if [ "$display_server" = "x11" ]; then
-	installList "x11"
-else
-	installList "wayland"
-fi
+installList $display_server
 
 install_eww
 
@@ -33,20 +28,16 @@ device=$(cat /sys/class/dmi/id/product_name)
 if echo "$device" | grep -q "MacBook"; then
 	print_yellow "MacBook detected"
 
-	# Install mbpfan
 	print_yellow "Installing mbpfan"
 	paru -S mbpfan-git
 
-	# System files
+	# Swap keys
 	print_yellow "Copying system files"
-	sudo cp ./system/Macbook/hid_apple.conf /etc/modprobe.d/hid_apple.conf
-	sudo cp ./system/Macbook/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+	sudo cp ./system/Macbook/hid_apple.conf 			/etc/modprobe.d/hid_apple.conf
 
-	# Services
 	print_yellow "Enabling services"
 	sudo systemctl enable mbpfan
 
-	# Regenerate initramfs
 	print_yellow "Regenerating initramfs"
 	# sudo dracut --regenerate-all --force              # Use this on non-Arch based distros
 	# mkinitcpio -p linux                               # For Arch, but didn't work for me so;
@@ -55,8 +46,9 @@ fi
 
 # System files
 print_yellow "Copying system files"
-sudo cp ./system/pacman.conf /etc/pacman.conf
-sudo cp ./system/skip-username.conf /etc/systemd/system/getty@tty1.service.d/skip-username.conf
+sudo cp ./system/pacman.conf 							/etc/pacman.conf
+sudo cp ./system/skip-username.conf 					/etc/systemd/system/getty@tty1.service.d/skip-username.conf
+sudo cp ./system/30-touchpad.conf 						/etc/X11/xorg.conf.d/30-touchpad.conf
 
 # Services
 print_yellow "Enabling services"
@@ -64,9 +56,9 @@ sudo systemctl enable NetworkManager
 
 # Custom Desktop Entries
 print_yellow "Copying desktop entries"
-sudo cp ./system/Desktop/syncthing.desktop /usr/share/applications/syncthing.desktop
-sudo cp ./system/Desktop/spotify_player.desktop /usr/share/applications/spotify_player.desktop
-sudo cp ./system/Desktop/org.rnd2.cpupower_gui.desktop /usr/share/applications/org.rnd2.cpupower_gui.desktop
+sudo cp ./system/Desktop/syncthing.desktop 				/usr/share/applications/syncthing.desktop
+sudo cp ./system/Desktop/spotify_player.desktop 		/usr/share/applications/spotify_player.desktop
+sudo cp ./system/Desktop/org.rnd2.cpupower_gui.desktop	/usr/share/applications/org.rnd2.cpupower_gui.desktop
 
 # Make scripts executable
 print_yellow "Making scripts executable"
